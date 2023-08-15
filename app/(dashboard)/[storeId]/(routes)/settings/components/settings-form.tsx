915f98e0a6d97e8e372087b2d4944a35b6edc121
lptrk <lktpriv@gmail.com>
2023-08-15 14:29:23 +0200

    renamed use oririn hook

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -16,7 +16,7 @@ import axios from "axios";
 import {useParams, useRouter} from "next/navigation";
 import {AlertModal} from "@/components/modals/alert-modal";
 import {ApiAlert} from "@/components/ui/api-alert";
-import {useOrigin} from "@/hooks/use-porigin";
+import {useOrigin} from "@/hooks/use-origin";
 
 interface SettingsFormProps {
     initialData: Store
